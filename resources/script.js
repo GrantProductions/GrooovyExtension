5d863dc50bf90a89bf65439a9570da614e0c7bd7
Grant <108592892+GrantProductions@users.noreply.github.com>
2022-07-15 13:10:19 -0400

    Add login / register validation, add review page WIP

--- resources/script.js
+++ resources/script.js
@@ -1,28 +1,225 @@
-if(chrome.tabs){
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        const parsedURL = new URL(tabs[0].url)
-        const url = parsedURL.protocol + '//' + parsedURL.hostname + parsedURL.pathname
-        $('#url').text(url).attr('title', url)
-    });
+const exampleTags = [
+  { name: "Entertainment", color: "#e6584e" },
+  { name: "Programming", color: "#2ba9e3" },
+  { name: "Cool", color: "#2ba9e3" },
+  { name: "For School", color: "#e32bb2" },
+];
+
+const skipIntroForDev = false;
+if (skipIntroForDev) {
+  $(".intro-container").hide();
 }
 
-$('.nav-item').on('click', function () {
-    $('.nav-item.active').removeClass('active')
-    $(this).addClass('active')
-    const key = $(this).data('key')
-    $('#nav-content .nav-content.active').removeClass('active')
-    $(`#nav-content .nav-content#${key}`).addClass('active')
+if (chrome.tabs) {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    const parsedURL = new URL(tabs[0].url);
+    const url =
+      parsedURL.protocol + "//" + parsedURL.hostname + parsedURL.pathname;
+    $("#url").text(url).attr("title", url);
+  });
+}
+
+$(".nav-item, .page-director").on("click", function () {
+  if ($(this).hasClass("nav-item")) {
+    $(".nav-item.active").removeClass("active");
+    $(this).addClass("active");
+  }
+  const key = $(this).data("key");
+  $("#nav-content .nav-content.active").removeClass("active");
+  $(`#nav-content .nav-content#${key}`).addClass("active");
+});
+
+$(".intro-container #intro-options h2").on("click", function () {
+  const key = $(this).data("key");
+  $(".intro").fadeOut(function () {
+    $(`.intro-option#${key}`).fadeIn();
+  });
+});
+
+$(".intro-option-back").on("click", function () {
+  $(".intro-option:visible").fadeOut(function () {
+    $(".intro").fadeIn();
+    $('.intro-option .error-label').text('')
+  });
+});
+
+const loginForm = {
+  email: $("#login-email"),
+  password: $("#login-password"),
+};
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+$("#login-button").on("click", function () {
+  const email = loginForm.email.val();
+  let shouldHideEmailError = true;
+  if (email.trim().length == 0) {
+    loginForm.email
+      .next(".error-label")
+      .text("Remember to include your email!");
+    shouldHideEmailError = false;
+  } else if (!emailRegex.test(email)) {
+    loginForm.email
+      .next(".error-label")
+      .text("Hmm... that email doesn't look right");
+    shouldHideEmailError = false;
+  }
+  if (shouldHideEmailError) {
+    loginForm.email.next(".error-label").text("");
+  }
+
+  const password = loginForm.password.val();
+  let shouldHidePasswordError = true;
+  if (password.length == 0) {
+    loginForm.password
+      .next(".error-label")
+      .text("Wait! You forgot a password!");
+    shouldHidePasswordError = false;
+  } else if (password.length < 6) {
+    loginForm.password
+      .next(".error-label")
+      .html("Your password must be at least<br/> 6 characters!");
+    shouldHidePasswordError = false;
+  }
+  if (shouldHidePasswordError) {
+    loginForm.password.next(".error-label").text("");
+  }
+
+  if (shouldHideEmailError && shouldHidePasswordError) {
+    $("#loading").show();
+    $("#click-block").show();
+    setTimeout(function () {
+      $("#loading").hide();
+      $("#click-block").hide();
+      let isAuthencated = true; //TODO
+      if (isAuthencated) {
+        $(".intro-container").fadeOut(function () {
+          $(".container").fadeIn();
+        });
+      }
+    }, 2000);
+  }
+});
+
+const registerForm = {
+  email: $("#register-email"),
+  password: $("#register-password"),
+  confirmPassword: $("#register-password-confirm"),
+};
+
+$("#register-button").on("click", function () {
+  const email = registerForm.email.val();
+  let shouldHideEmailError = true;
+  if (email.trim().length == 0) {
+    registerForm.email
+      .next(".error-label")
+      .text("Remember to include your email!");
+    shouldHideEmailError = false;
+  } else if (!emailRegex.test(email)) {
+    registerForm.email
+      .next(".error-label")
+      .text("Hmm... that email doesn't look right");
+    shouldHideEmailError = false;
+  }
+  if (shouldHideEmailError) {
+    registerForm.email.next(".error-label").text("");
+  }
+
+  const password = registerForm.password.val();
+  let shouldHidePasswordError = true;
+  if (password.length == 0) {
+    registerForm.password
+      .next(".error-label")
+      .text("Wait! You forgot a password!");
+    shouldHidePasswordError = false;
+  } else if (password.length < 6) {
+    registerForm.password
+      .next(".error-label")
+      .html("Your password must be at least<br/> 6 characters!");
+    shouldHidePasswordError = false;
+  }
+  if (shouldHidePasswordError) {
+    registerForm.password.next(".error-label").text("");
+  }
+
+  const confirmPassword = registerForm.confirmPassword.val();
+  let shouldHideConfirmPasswordError = true;
+  if (shouldHidePasswordError && confirmPassword != password) {
+    registerForm.confirmPassword
+      .next(".error-label")
+      .text("Passwords aren't the same!");
+    shouldHideConfirmPasswordError = false;
+  }
+  if (shouldHideConfirmPasswordError) {
+    registerForm.confirmPassword.next(".error-label").text("");
+  }
+
+  if (
+    shouldHideEmailError &&
+    shouldHidePasswordError &&
+    shouldHideConfirmPasswordError
+  ) {
+    $("#loading").show();
+    $("#click-block").show();
+    setTimeout(function () {
+      $("#loading").hide();
+      $("#click-block").hide();
+      let isRegistered = true; //TODO
+      if (isRegistered) {
+        $(".intro-container").fadeOut(function () {
+          $(".container").fadeIn();
+        });
+      }
+    }, 2000);
+  }
+});
+
+$("#click-block").click(false);
+
+const stars = $("#edit-review .stars .material-icons");
+
+stars.on("click", function () {
+  $(this)
+    .html("star")
+    .addClass("selected")
+    .prevAll()
+    .html("star")
+    .addClass("selected");
+  $(this).nextAll().html("star_border").removeClass("selected");
+});
+
+$("#edit-review #review-text").on("input", function () {
+  $(this).css("height", "1px");
+  $(this).css("height", 21 + $(this).prop("scrollHeight") + "px");
+});
+
+const addTagPopup = $("#add-tag-popup");
+const addTagList = $("#add-tag-list");
+
+$(addTagList).on('click', '.tag', function(){
+    $(this).find('.material-icons').toggle()
 })
 
-$('.intro-container #intro-options h2').on('click', function(){
-    const key = $(this).data('key')
-    $('.intro').fadeOut(function(){
-        $(`.intro-option#${key}`).fadeIn(); 
-    })
-})
+addTagPopup.on("click", function (e) {
+  e.stopPropagation();
+});
 
-$('.intro-option-back').on('click', function(){
-    $(".intro-option:visible").fadeOut(function(){
-        $('.intro').fadeIn()
-    })
-})
+$("#edit-review #review-tags #add-tag").on("click", function (e) {
+  addTagPopup.show();
+  addTagList.empty();
+  exampleTags.forEach((e) => {
+    addTagList.append(`
+            <div class="tag">
+                <span class="material-icons selected-tag">done</span>
+                ${e.name}
+            </div>
+        `);
+  });
+  e.stopPropagation();
+});
+
+$(document).on("click", function (e) {
+  if (!e.target.closest("#add-tag-popup")) {
+    addTagPopup.hide();
+  }
+});
