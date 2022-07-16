784da3ba7ef416652ab1139f932e6ebd5322ccf3
Grant <108592892+GrantProductions@users.noreply.github.com>
2022-07-16 02:20:19 -0400

    Finish reviews page, review tags WIP

--- resources/script.js
+++ resources/script.js
@@ -10,7 +10,24 @@ if (!chrome.storage) {
 }
 
 let jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYWFhIiwiaXNzIjoiR3Jvb292eSIsImV4cCI6MTY1NzQ0MjA3NH0.PWj713JXouUPnd98iIwra43uWQnYUr0GywRTy755FYg";
+let currentURL;
 
+if (chrome.tabs) {
+
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    const parsedURL = new URL(tabs[0].url);
+    const url =
+      parsedURL.protocol + "//" + parsedURL.hostname + parsedURL.pathname;
+    currentURL = url;
+    $("#url").text(url).attr("title", url);
+  })
+} else {
+  currentURL = "https://www.google.com"
+  $("#url").text(currentURL).attr("title", currentURL);
+  $(document).ready(function () {
+    initHomepage();
+  })
+}
 chrome.storage.sync.get(['jwtToken', 'username'], function (result) {
   if (result.jwtToken) {
     const now = Date.now()
@@ -20,7 +37,7 @@ chrome.storage.sync.get(['jwtToken', 'username'], function (result) {
       console.log(`jwt token still valid for ${(expiry - now) / 1000 / 60} minutes`)
       $('.intro-container').hide()
       jwtToken = result.jwtToken;
-      initHomepage(result.username, result.jwtToken)
+      initHomepage(result.username)
     } else {
       console.log(`jwt token expired ${(now - expiry) / 1000 / 60} minutes ago`)
     }
@@ -32,6 +49,9 @@ if (skipIntroForDev) {
   $(".intro-container").hide();
 }
 
+const navItemInitFunctions = {
+  "reviews": loadSiteReviews
+}
 
 $(".nav-item, .page-director").on("click", function () {
   if ($(this).hasClass("nav-item")) {
@@ -39,6 +59,9 @@ $(".nav-item, .page-director").on("click", function () {
     $(this).addClass("active");
   }
   const key = $(this).data("key");
+  if(navItemInitFunctions[key]){
+    navItemInitFunctions[key]();
+  }
   $("#nav-content .nav-content.active").removeClass("active");
   $(`#nav-content .nav-content#${key}`).addClass("active");
 });
@@ -50,7 +73,7 @@ $(".intro-container #intro-options h2").on("click", function () {
   });
 });
 
-$(".intro-option-back").on("click", function () {
+$(".intro-container .back-director").on("click", function () {
   $(".intro-option:visible").fadeOut(function () {
     $(".intro").fadeIn();
     $('.intro-option .error-label').text('')
@@ -102,19 +125,19 @@ $("#login-button").on("click", function () {
     setTimeout(function () {
       $.ajax({
         url: endpointURL + 'authenticate',
-        type: "POST",
+        method: "POST",
         data: {
           username: username,
           password: password,
         },
         success: function (data) {
-          console.log('success', data)
           const jwt = data.token;
           chrome.storage.sync.set({ jwtToken: jwt, username }, function () {
             finishLoading()
+            jwtToken = jwt;
             $(".intro-container").fadeOut(function () {
               $(".container").fadeIn();
-              initHomepage(username, jwt)
+              initHomepage(username)
             });
           });
         }, error: function (data) {
@@ -219,7 +242,7 @@ $("#register-button").on("click", function () {
     setTimeout(function () {
       $.ajax({
         url: endpointURL + 'register',
-        type: "POST",
+        method: "POST",
         data: {
           username: username,
           email: email,
@@ -252,6 +275,11 @@ $("#click-block").click(false);
 
 /* add tag */
 
+const editReviewSection = $('#edit-review')
+const reviewsSection = $('#reviews')
+
+const navContentArea = $('#nav-content')
+
 const stars = $("#edit-review .stars .material-icons");
 
 let selectedStars = 0;
@@ -266,7 +294,9 @@ stars.on("click", function () {
   selectedStars = prev.length + 1;
 });
 
-$("#edit-review #review-text").on("input", function () {
+const reviewText = $("#edit-review #review-text")
+
+reviewText.on("input", function () {
   $(this).css("height", "1px");
   $(this).css("height", 21 + $(this).prop("scrollHeight") + "px");
 });
@@ -279,14 +309,47 @@ $(addTagList).on('click', '.tag', function () {
 
 const reviewErrorLabel = $('#review-error')
 $('#edit-review #submit-review').on('click', function () {
+  reviewErrorLabel.text('')
   let shouldHideReviewError = true;
+  const activePrivacyToggle = $('#edit-review #review-footer .privacy-toggle .privacy-option.active')
   if (selectedStars == 0) {
     reviewErrorLabel.text("Don't forget to choose a rating!")
+    shouldHideReviewError = false;
+  } else if (activePrivacyToggle.length == 0) {
+    reviewErrorLabel.text("Please choose a privacy option for your review below")
     shouldHideReviewError = false;
   }
   if (shouldHideReviewError) {
     reviewErrorLabel.text('')
-    startLoading();
+    startLoading(editReviewSection);
+    setTimeout(function () {
+      $.ajax({
+        url: endpointURL + 'reviews/new',
+        method: "POST",
+        data: {
+          url: currentURL,
+          stars: selectedStars,
+          text: reviewText.val(),
+          tagIDs: $('#edit-review .tags .tag').map(function () { return $(this).data('id') }).toArray(),
+          isPrivate: activePrivacyToggle.data('def') == 'private'
+        },
+        headers: { "Authorization": "Bearer " + jwtToken },
+        success: function (data) {
+          finishLoading(editReviewSection)
+          if (data.success) {
+            showSuccessMessage("Review posted", 5000)
+            editReviewSection.removeClass('active');
+            reviewsSection.addClass('active')
+            loadSiteReviews()
+          }
+        },
+        error: function (data) {
+          finishLoading(editReviewSection)
+          reviewErrorLabel.text(data.responseJSON.message)
+        }
+
+      })
+    }, 500)
   }
 })
 
@@ -346,12 +409,12 @@ addTagInput.on('input', function () {
                   color: invertedColor,
                   backgroundColor: '#' + e.color,
                   borderColor: invertedColor
-                })
+                }).data('id', e.id)
                 tagSuggestions.append(tagElem)
                 displayedTags++;
               }
             })
-            if(displayedTags == 0){
+            if (displayedTags == 0) {
               tagSuggestions.html('<span class="input-suggestion-label">No suggestions.</span>')
             }
           }
@@ -369,7 +432,7 @@ addTagInput.on('input', function () {
 const reviewTags = $('#edit-review .tags')
 
 tagSuggestions.on('click', '.tag', function (e) {
-  if(!!reviewTags.has('i.tags-label').length){
+  if (!!reviewTags.has('i.tags-label').length) {
     reviewTags.find('i.tags-label').hide()
   }
   const tag = $(e.target.closest('.tag'))
@@ -380,15 +443,15 @@ tagSuggestions.on('click', '.tag', function (e) {
   addTagInput.val('')
 })
 
-reviewTags.on('click', '.tag .material-icons.delete-tag', function(e){
+reviewTags.on('click', '.tag .material-icons.delete-tag', function (e) {
   const tag = $(e.target.closest('.tag'));
   tag.remove();
-  if($('.tag', reviewTags).length == 0){
+  if ($('.tag', reviewTags).length == 0) {
     reviewTags.find('i.tags-label').show()
   }
 })
 
-$(document).on('click', '.new-tag-suggestion', function(e){
+$(document).on('click', '.new-tag-suggestion', function (e) {
   newTagPopup.show();
   e.stopPropagation()
 })
@@ -412,9 +475,8 @@ tagColorCustomHex.on('input', function () {
   tagColorActiveIndicator.remove()
 })
 
-const privacyOptions = $('#new-tag #new-tag-popup .new-tag-privacy-option')
-privacyOptions.on('click', function () {
-  privacyOptions.removeClass('active');
+$('.privacy-option').on('click', function () {
+  $(this).parent('.privacy-toggle').find('.privacy-option').removeClass('active');
   $(this).addClass('active')
 })
 
@@ -445,7 +507,7 @@ $('#new-tag-button').on('click', function () {
   } else if (!tagColorActiveIndicator.is(':visible') && customHex.trim().length == 0) {
     newTagErrorLabel.text(`Don't forget a tag color!`)
     shouldHideNewTagError = false;
-  } else if (!privacyOptions.is('.active')) {
+  } else if ($('#new-tag').has('.privacy-option.active').length == 0) {
     newTagErrorLabel.text(`Please choose whether the tag is public/private`)
     shouldHideNewTagError = false;
   } else if (!hexRegex.test(tagColorCustomHex.val())) {
@@ -463,7 +525,7 @@ $('#new-tag-button').on('click', function () {
         data: {
           name: newTagName,
           color: tagColorCustomHex.val().trim(),
-          isPrivate: $('.new-tag-privacy-option.active').data('def') == 'private'
+          isPrivate: $('#new-tag-popup .privacy-option.active').data('def') == 'private'
         },
         headers: {
           "Authorization": "Bearer " + jwtToken
@@ -475,7 +537,8 @@ $('#new-tag-button').on('click', function () {
             newTagNameInput.val('')
             tagColorCustomHex.val('')
             tagColorActiveIndicator.remove()
-            $('.new-tag-privacy-option.active').removeClass('active')
+            $('#new-tag-popup .privacy-option.active').removeClass('active')
+            showSuccessMessage(`Created new tag '${newTagName}'`, 5000)
           }
         },
         error: function (data) {
@@ -489,35 +552,155 @@ $('#new-tag-button').on('click', function () {
 
 /* boot up */
 
-function initHomepage(username, jwt) {
+function initHomepage(username) {
   $('#profile-name').text(username)
-  loadSiteReviews(jwt)
+  loadSiteReviews(jwtToken)
 }
 
-function loadSiteReviews(jwt) {
-  startLoading()
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    const parsedURL = new URL(tabs[0].url);
-    const url =
-      parsedURL.protocol + "//" + parsedURL.hostname + parsedURL.pathname;
-    $("#url").text(url).attr("title", url);
+/* site reviews section */
+
+const ratingAverageLabel = $('#reviews .rating-container .rating .rating-value')
+
+function loadSiteReviews(sortOption, callbackOnFinish) {
+  startLoading(navContentArea)
+  reviewsSection.hide();
+  setTimeout(function () {
+    const requestData = {
+      url: currentURL
+    }
+    if(sortOption){
+      requestData.sortOption = sortOption
+    }
     $.ajax({
       url: endpointURL + 'reviews',
       type: "GET",
-      data: { url },
-      headers: { "Authorization": "Bearer " + jwt },
+      data: requestData,
+      headers: { "Authorization": "Bearer " + jwtToken },
       success: function (data) {
-        console.log(data);
-        finishLoading()
+        handleSiteReviewData(data.data)
+        finishLoading(navContentArea)
+        reviewsSection.show()
+        if(callbackOnFinish){
+          callbackOnFinish(true)
+        }
       },
       error: function (data) {
-        console.log(data)
-        console.log(JSON.parse(data.responseText));
         finishLoading()
+        showErrorMessage("Failed to load website reviews. Try closing & reopening this extension", 10000)
+        if(callbackOnFinish){
+          callbackOnFinish(false)
+        }
       }
     })
-  });
+  }, 500)
 }
+
+const reviews = $('#reviews .reviews')
+const reviewsCountLabel = $('#reviews-count')
+const websiteReviewStars = $('#reviews .rating-container .rating .rating-stars .material-icons')
+
+function handleSiteReviewData(data) {
+  const now = Date.now();
+  reviews.empty()
+
+  const totalReviews = data.totalReviews
+  reviewsCountLabel.text(conditionallyPlurify(totalReviews, 'review'))
+  const averageRating = parseFloat(data.averageRating).toFixed(1)
+  ratingAverageLabel.text(averageRating)
+
+  const ratingBarPropNames = ["fives", "fours", "threes", "twos", "ones"]
+  ratingBarPropNames.forEach(prop => {
+    const barWidth = ((data[prop] / totalReviews) * 100).toFixed(2)
+    $(`#reviews .rating-container .rating-distribution .rating-bar[data-def="${prop}"] > div`).css('width', barWidth + '%');
+  })
+
+  websiteReviewStars.html('star_border')
+  const numOfFullStars = Math.floor(data.averageRating)
+  const firstFullStars = $('#reviews .rating-container .rating .rating-stars .material-icons').slice(0, numOfFullStars)
+  firstFullStars.html('star')
+  if ((data.averageRating - numOfFullStars) >= 0.5) {
+    firstFullStars.last().next('.material-icons').html('star_half')
+  }
+  console.log(data)
+  data.reviews.forEach(review => {
+    const reviewElement = $(`<div class="review"><div class="review-metadata"><div class="review-author"></div><div class="review-date"></div></div><div class="review-stars"><span class="material-icons">star_border</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span><span class="material-icons">star_border</span></div><div class="review-message"></div></div>`);
+    reviewElement.find('.review-metadata .review-author').text(review.author.username)
+
+    const timeDifferenceMs = now - (review.createdDateTime * 1000)
+    const timeDifferenceReadable = convertMsToReadable(timeDifferenceMs)
+
+    const reviewDateElement = reviewElement.find('.review-metadata .review-date')
+    reviewDateElement.text(timeDifferenceReadable)
+    reviewDateElement.on('mouseenter', function () {
+      $(this).text(review.formattedDateTime)
+    })
+
+    let prevTimeout;
+    reviewDateElement.on('mouseleave', function () {
+      clearTimeout(prevTimeout)
+      prevTimeout = setTimeout(() => {
+        $(this).text(timeDifferenceReadable)
+      }, 500)
+    })
+
+    reviewElement.find('.review-stars .material-icons').slice(0, review.stars).html('star')
+
+
+    reviewElement.find('.review-message').text(review.text)
+    reviews.append(reviewElement)
+  })
+}
+
+/* reviews sort */
+
+const reviewsSortPopup = $('#reviews-sort-popup')
+const reviewsSortOptions = $('.reviews-sort-option', reviewsSortPopup)
+
+reviewsSortPopup.on('click', function (e) {
+  e.stopPropagation();
+})
+
+reviewsSortOptions.on('click', function(){
+  const option = $(this).find('.reviews-sort-option-label').text().trim().toLowerCase().trim()
+  loadSiteReviews(option, function(didSucceed){
+    if(didSucceed){
+      reviewsSortOptions.find('.material-icons').hide();
+      reviewsSortOptions.filter(`[data-def="${option}"]`).find('.material-icons').show()
+      console.log(reviewsSortOptions.filter(`[data-def="${option}"]`))
+    }
+  })
+  closeExpandablePopup(reviewsSortPopup)
+})
+
+/* general */
+
+function convertMsToReadable(ms) {
+  const seconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 30);
+  const year = Math.floor(days / 365);
+
+  if (year != 0) {
+    return conditionallyPlurify(year, 'year') + ' ago'
+  } else if (weeks != 0) {
+    return conditionallyPlurify(weeks, 'week') + ' ago'
+  } else if (days != 0) {
+    return conditionallyPlurify(days, 'day') + ' ago'
+  } else if (hours != 0) {
+    return conditionallyPlurify(hours, 'hour') + ' ago'
+  } else if (minutes != 0) {
+    return conditionallyPlurify(minutes, 'minute') + ' ago'
+  } else {
+    return 'a few seconds ago'
+  }
+}
+
+function conditionallyPlurify(num, label) {
+  return `${num} ${label}${num != 1 ? 's' : ''}`
+}
+
 
 function startLoading(element) {
   $('#loading').show().appendTo(element);
@@ -527,6 +710,26 @@ function startLoading(element) {
 function finishLoading(element) {
   $('#loading').hide().appendTo(element);
   $('#click-block').hide().appendTo(element);
+}
+
+$(document).on('click', '.expandable-popup-trigger', function (e) {
+  const key = $(this).data('key')
+  const popupElement = $(`.expandable-popup#${key}`)
+  const [desiredWidth, desiredHeight] = [popupElement.data('width'), popupElement.data('height')]
+  popupElement.css('display', 'flex').animate({
+    width: desiredWidth,
+    height: desiredHeight
+  }, 300)
+  e.stopPropagation();
+})
+
+function closeExpandablePopup(popup){
+  popup.fadeOut(100, function(){
+    popup.css({
+      width: 0,
+      height: 0
+    })
+  })
 }
 
 /* profile controls */
@@ -556,7 +759,45 @@ $(document).on("click", function (e) {
   if (!e.target.closest('#profile-popup')) {
     profilePopup.hide()
   }
+
   if (!e.target.closest('#new-tag-popup')) {
     newTagPopup.hide()
   }
+
+  if (!e.target.closest('#reviews-sort-popup')) {
+    closeExpandablePopup(reviewsSortPopup)
+  }
 });
+
+/* notifications */
+
+const notifications = $('#notifications')
+
+
+function showSuccessMessage(message, timeout) {
+  showMessage(message, timeout, 'check', 'green')
+}
+
+function showErrorMessage(message, timeout) {
+  showMessage(message, timeout, 'priority_high', 'red')
+}
+
+const notificationElement = $(`<div class="notification"><div class="icon"><span class="material-icons type-icon"></span></div><div class="message"></div><span class="material-icons notification-close">close</span></div>`)
+function showMessage(message, timeout, iconCode, iconBg) {
+  const notification = notificationElement.clone();
+  notification.find('.message')
+    .text(message)
+  notification.find('.icon')
+    .css('background-color', iconBg)
+    .find('span.type-icon').html(iconCode);
+  notification.find('.notification-close').on('click', function () {
+    notification.remove();
+  })
+  notification.hide().prependTo(notifications)
+  notification.fadeIn();
+  setTimeout(function () {
+    notification.fadeOut(function () {
+      notification.remove()
+    })
+  }, timeout)
+}
